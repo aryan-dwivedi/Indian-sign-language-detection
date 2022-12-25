7c7e99a4bef44b1c5765b82123672cec4813644f
Aryan Dwivedi <Aryan Dwivedi>
2022-12-25 20:03:13 +0530

    3rd commit

--- A2SL/views.py
+++ A2SL/views.py
@@ -17,10 +17,6 @@ def about_view(request):
 	return render(request,'about.html')
 
 
-def contact_view(request):
-	return render(request,'contact.html')
-
-@login_required(login_url="login")
 def animation_view(request):
 	if request.method == 'POST':
 		text = request.POST.get('sen')
@@ -103,40 +99,3 @@ def animation_view(request):
 		return render(request,'animation.html',{'words':words,'text':text})
 	else:
 		return render(request,'animation.html')
-
-
-
-
-def signup_view(request):
-	if request.method == 'POST':
-		form = UserCreationForm(request.POST)
-		if form.is_valid():
-			user = form.save()
-			login(request,user)
-			# log the user in
-			return redirect('animation')
-	else:
-		form = UserCreationForm()
-	return render(request,'signup.html',{'form':form})
-
-
-
-def login_view(request):
-	if request.method == 'POST':
-		form = AuthenticationForm(data=request.POST)
-		if form.is_valid():
-			#log in user
-			user = form.get_user()
-			login(request,user)
-			if 'next' in request.POST:
-				return redirect(request.POST.get('next'))
-			else:
-				return redirect('animation')
-	else:
-		form = AuthenticationForm()
-	return render(request,'login.html',{'form':form})
-
-
-def logout_view(request):
-	logout(request)
-	return redirect("home")
