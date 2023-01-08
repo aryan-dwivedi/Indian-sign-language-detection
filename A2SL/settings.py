760c1ff2eb365f088c9775f2edb9eec757835e89
Aryan Dwivedi <Aryan Dwivedi>
2023-01-08 19:43:26 +0530

    debug true

--- A2SL/settings.py
+++ A2SL/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '3k7=!d39#4@_&5a6to&4=_=j(c^v0(vv91cj5+9e8+d4&+01jb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
